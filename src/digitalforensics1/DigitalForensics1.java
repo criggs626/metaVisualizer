104ccf6a31aa7dd343e68edde3210f1505472298
Caleb Riggs <criggs2892@flpoly.org>
2017-10-10 18:37:11 -0400

    Added map view

--- src/digitalforensics1/DigitalForensics1.java
+++ src/digitalforensics1/DigitalForensics1.java
@@ -1,29 +1,58 @@
 package digitalforensics1;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.lang.GeoLocation;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
-import com.drew.metadata.exif.GpsDirectory;
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.Date;
-import java.util.Scanner;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-public class DigitalForensics1 {
+public class DigitalForensics1 extends Application {
 
     public static void main(String[] args) {
-        try {
-            File[] files = readFiles("D:/Phone/Auto Classic/");
-            image[] images=new image[files.length];
-            for (int i=0;i<files.length;i++) {
-                images[i]=new image(files[i]);
-                images[i].display();
-            }
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Meta Visualizer");
+
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select the folder with the images");
+        File directory = chooser.showDialog(primaryStage);
+
+        File[] files = readFiles(directory.toString());
+        image[] images = new image[files.length];
+        for (int i = 0; i < files.length; i++) {
+            images[i] = new image(files[i]);
+            images[i].display();
+        }
+
+        MyBrowser myBrowser = new MyBrowser();
+        StackPane root = new StackPane();
+        root.getChildren().add(myBrowser);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    class MyBrowser extends Region {
+
+        public MyBrowser() {
+            WebView webView = new WebView();
+            WebEngine webEngine = webView.getEngine();
+            final URL urlGoogleMaps = getClass().getResource("maps.html");
+            webEngine.load(urlGoogleMaps.toExternalForm());
+            webEngine.setJavaScriptEnabled(true);
+            webEngine.executeScript("var marker = new google.maps.Marker({position: {lat: 28.04550827777778, lng: -81.95190458333333},map: map,title: 'Hello World!'});");
+
+            getChildren().add(webView);
+
         }
     }
 
--- src/digitalforensics1/image.java
+++ src/digitalforensics1/image.java
@@ -1,28 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package digitalforensics1;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.lang.GeoLocation;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.GpsDirectory;
 import java.io.File;
 import java.util.Date;
 
-/**
- *
- * @author Caleb
- */
 public class image {
 
     File file;
     Metadata meta;
     Date date;
     GeoLocation location;
+    String phone;
 
     public image(File in) {
         try {
@@ -35,14 +28,18 @@ public class image {
 
             GpsDirectory gps = meta.getDirectory(GpsDirectory.class);
             location = gps.getGeoLocation();
+            
+            ExifIFD0Directory exif0= meta.getDirectory(ExifIFD0Directory.class);
+            phone=exif0.getString(ExifIFD0Directory.TAG_MAKE)+" "+exif0.getString(ExifIFD0Directory.TAG_MODEL);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
     }
-    
-    public void display(){
+
+    public void display() {
         System.out.println(file.getName());
         System.out.println(date.toString());
-        System.out.println(location.toString()+"\n---------\n");
+        System.out.println(location.toString());
+        System.out.println(phone + "\n---------\n");
     }
 }
